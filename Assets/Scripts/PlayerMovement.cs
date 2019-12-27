59d340a56ec6bfabf7f8272b788a5fe28aa068d3
Gaurav Sharma <sharma.gaurav1881@gmail.com>
2019-12-26 23:25:17 -0500

    added jump animation

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public CharacterController2D controller;
+    public float jumpVelocity = 40f;
+
+    public Animator animator;
+
+    bool jump = false;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+            animator.SetBool("isJumping", true);
+        }
+    }
+
+    void FixedUpdate ()
+    {
+        controller.Move(0, false, jump);
+        jump = false;
+    }
+
+    public void onLanding()
+    {
+        animator.SetBool("isJumping", false);
+    }
+}
